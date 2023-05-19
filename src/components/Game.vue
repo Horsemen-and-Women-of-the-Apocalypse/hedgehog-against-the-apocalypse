389413545cd4d5d2e61796e75ab2e56f17d5d8a5
tom.mansion <tomansion@yahoo.fr>
2023-05-19 20:26:51 +0200

    Clean game component

---
+++ src/components/Game.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="game">
+    game
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hata-Game',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
