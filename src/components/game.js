3a66aecd98555a80f2f9de4647adfd02ba2ba828
tom.mansion <tomansion@yahoo.fr>
2023-05-19 20:55:44 +0200

    Resize on widow size change OK

--- src/components/game.js
+++ src/components/game.js
@@ -1,24 +1,28 @@
 import Phaser from "phaser";
 
-// create Phaser.Game object assigned to global variable named game
-
 class BoardScene extends Phaser.Scene {
   constructor() {
     super("BoardScene");
+  }
+
+  create() {
+    // Add your game logic and scene setup here
   }
 }
 
 const config = {
   type: Phaser.AUTO,
-  width: "100vw",
-  height: "100vh",
+  width: window.innerWidth,
+  height: window.innerHeight,
   scene: BoardScene
 };
 
 const game = new Phaser.Game(config);
 
-
-// Set full screen
-
+// Set up resize event listener to adapt to window size changes
+window.addEventListener("resize", () => {
+  const { innerWidth, innerHeight } = window;
+  game.scale.resize(innerWidth, innerHeight);
+});
 
 export { game };
