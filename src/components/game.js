ab9377d8e2d658dfa5b3066bd534e0e55b438dd3
tom.mansion <tomansion@yahoo.fr>
2023-05-19 21:45:36 +0200

    WIP tileset

--- src/components/game.js
+++ src/components/game.js
@@ -1,12 +1,42 @@
 import Phaser from "phaser";
 
+
+const tilesets = {
+  tiles: {
+    name: "tiles",
+    path: "/assets/sprites/grass.png",
+    nbImagesPerRow: 1,
+    imageSize: [32, 32]
+  },
+};
+
+const TILESET = tilesets.tiles;
+
+
 class BoardScene extends Phaser.Scene {
+  MAP_SIZE = [25, 20];
+  map = null
+
   constructor() {
     super("BoardScene");
+  }
+  preload() {
+    // Load tileset
+    this.load.spritesheet(TILESET.name, TILESET.path, {
+      frameWidth: TILESET.imageSize[0],
+      frameHeight: TILESET.imageSize[1]
+    });
   }
 
   create() {
     // Add your game logic and scene setup here
+    // Create map
+    this.map = this.make.tilemap({
+      tileWidth: TILESET.imageSize[0],
+      tileHeight: TILESET.imageSize[1],
+      width: this.MAP_SIZE[0],
+      height: this.MAP_SIZE[1]
+    });
   }
 }
 
